8c1de52269ef090c2a2d6b96e32e228c74001b2b
Johan Trieloff <johan@trieloff.net>
2024-03-08 13:32:35 +0100

    different package

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/FreiFahren/backend/api"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
-	e.GET("/id", GetStation)
+	e.GET("/id", api.GetStation)
 
 	e.Start(":8080")
 }
